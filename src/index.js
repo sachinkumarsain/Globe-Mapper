e0b7b3b832c2fa5e35e2f047b76b806f8fdafebf
sachin <sachinsain21082004@gmail.com>
2023-08-02 18:13:04 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CountryStateCities from "./Country-State-cites/CountryStateCities"
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+        <CountryStateCities />
+    </>
+);
+
